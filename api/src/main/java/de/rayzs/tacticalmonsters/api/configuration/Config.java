608bb93429e5a1254e8800e19cd3cbee63ec2993
rayzsyt <alihan290404@gmail.com>
2026-02-03 18:26:15 +0100

    Add comments

--- api/src/main/java/de/rayzs/tacticalmonsters/api/configuration/Config.java
+++ api/src/main/java/de/rayzs/tacticalmonsters/api/configuration/Config.java
@@ -4,21 +4,126 @@ import java.util.Collection;
 
 public interface Config {
 
+    /**
+     * Reloads the config.
+     */
     void reload();
+
+    /**
+     * Saves the config changes.
+     */
     void save();
 
+    /**
+     * Sets a value.
+     *
+     * @param path Path to the section.
+     * @param target Target key.
+     * @param object Value to set.
+     * @return The current Config instance.
+     */
     Config set(String path, String target, Object object);
+
+    /**
+     * Sets a value.
+     *
+     * @param target Target key.
+     * @param object Value to set.
+     * @return The current Config instance.
+     */
     Config set(String target, Object object);
 
+    /**
+     * Sets a value and saves the config.
+     *
+     * @param path Path to the section.
+     * @param target Target key.
+     * @param object Value to set.
+     * @return The current Config instance.
+     */
     Config setAndSave(String path, String target, Object object);
+
+    /**
+     * Sets a value and saves the config.
+     *
+     * @param target Target key.
+     * @param object Value to set.
+     * @return The current Config instance.
+     */
     Config setAndSave(String target, Object object);
 
-    <T> T getOrSet(String path, String target, T t);
-    <T> T getOrSet(String target, T t);
+    /**
+     * Gets a value if it exists.
+     * Returns assigned default value and sets it in the config otherwise.
+     *
+     * @param path The path to the section.
+     * @param target The target key.
+     * @param value Default value.
+     * @return The existing or default value.
+     */
+    <T> T getOrSet(String path, String target, T value);
 
+    /**
+     * Gets a value if it exists.
+     * Returns assigned default value and sets it in the config otherwise.
+     *
+     * @param target The target key.
+     * @param value Default value.
+     * @return The existing or default value.
+     */
+    <T> T getOrSet(String target, T value);
+
+    /**
+     * Gets a value.
+     *
+     * @param target Target key.
+     * @return The value.
+     */
     Object get(String target);
+
+    /**
+     * Gets a value.
+     *
+     * @param path Path to the section.
+     * @param target Target key.
+     * @return The value.
+     */
     Object get(String path, String target);
 
+    /**
+     * Gets all keys.
+     *
+     * <pre>
+     *     {@code
+     *     Collection<String> keys = getKeys(false);
+     *     // Example: ["monster1", "monster2", "settings"]
+     *
+     *     Collection<String> keys = getKeys(true);
+     *     // Example: ["monster1", "monster1.hp", "monster1.items", "monster1.items.mainhand", "monster2"...]
+     *     }
+     * </pre>
+     *
+     * @param deep Whether to get keys deeply.
+     * @return Collection of keys.
+     */
     Collection<String> getKeys(boolean deep);
+
+    /**
+     * Gets all keys from a section.
+     *
+     * <pre>
+     *     {@code
+     *     Collection<String> keys = getKeys("monster1", false);
+     *     // Example: ["hp", "items"]
+     *
+     *     Collection<String> keys = getKeys("monster1", true);
+     *     // Example: ["hp", "items", "items.mainhand"]
+     *     }
+     * </pre>
+     *
+     * @param section The section to get keys from.
+     * @param deep Whether to get keys deeply.
+     * @return Collection of keys.
+     */
     Collection<String> getKeys(String section, boolean deep);
 }
--- api/src/main/java/de/rayzs/tacticalmonsters/api/configuration/ConfigProvider.java
+++ api/src/main/java/de/rayzs/tacticalmonsters/api/configuration/ConfigProvider.java
@@ -1,5 +1,13 @@
 package de.rayzs.tacticalmonsters.api.configuration;
 
 public interface ConfigProvider {
+
+    /**
+     * Gets an already existing Config instance or creates a new one.
+     *
+     * @param filePath Path to the config file.
+     * @param fileName Name of the config file.
+     * @return The existing or newly created Config instance.
+     */
     Config getOrCreate(String filePath, String fileName);
 }
