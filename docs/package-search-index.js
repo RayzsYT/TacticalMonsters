745d7a93b985982cd4dbb466f2ce1153761f2cb1
rayzsyt <alihan290404@gmail.com>
2026-02-03 20:06:04 +0100

    Add doc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"de.rayzs.tacticalmonsters.api"},{"l":"de.rayzs.tacticalmonsters.api.attack"},{"l":"de.rayzs.tacticalmonsters.api.configuration"},{"l":"de.rayzs.tacticalmonsters.api.scheduler"}];updateSearchResults();
