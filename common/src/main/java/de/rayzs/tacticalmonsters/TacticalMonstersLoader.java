e7ff44d853d103371d86cb1b40445dd27167daab
rayzsyt <alihan290404@gmail.com>
2026-02-03 22:03:59 +0100

    Unregister on unload

--- common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
+++ common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
@@ -5,15 +5,18 @@ import de.rayzs.tacticalmonsters.attacks.WitherSkeletonAttack;
 import de.rayzs.tacticalmonsters.impl.TacticalMonstersImpl;
 import de.rayzs.tacticalmonsters.listener.MonsterHandler;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TacticalMonstersLoader extends JavaPlugin {
 
+    private TacticalMonstersImpl api;
+
     @Override
     public void onEnable() {
-        final TacticalMonstersImpl api = new TacticalMonstersImpl(this);
+        api = new TacticalMonstersImpl(this);
         TacticalMonsters.setInstance(api);
 
 
@@ -26,6 +29,7 @@ public class TacticalMonstersLoader extends JavaPlugin {
 
     @Override
     public void onDisable() {
-
+        HandlerList.unregisterAll(this);
+        api.unregisterAllAttacks();
     }
 }
