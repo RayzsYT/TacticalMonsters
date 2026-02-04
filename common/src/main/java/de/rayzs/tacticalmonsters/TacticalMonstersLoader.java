67049b03694e458f9a81c4d4d03199253e283f2c
rayzsyt <alihan290404@gmail.com>
2026-02-04 15:53:58 +0100

    Implement reload from API

--- common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
+++ common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
@@ -1,19 +1,14 @@
 package de.rayzs.tacticalmonsters;
 
-import de.rayzs.tacticalmonsters.api.TacticalMonsters;
+import de.rayzs.tacticalmonsters.attacks.WitherSkeletonAttack;
+import de.rayzs.tacticalmonsters.impl.TacticalMonstersImpl;
 import de.rayzs.tacticalmonsters.api.helper.VersionHelper;
 import de.rayzs.tacticalmonsters.attacks.SkeletonAttack;
 import de.rayzs.tacticalmonsters.attacks.SpiderAttack;
-import de.rayzs.tacticalmonsters.attacks.WitherSkeletonAttack;
 import de.rayzs.tacticalmonsters.attacks.ZombieAttack;
-import de.rayzs.tacticalmonsters.impl.TacticalMonstersImpl;
-import de.rayzs.tacticalmonsters.listener.AntiWitherCheeseHandler;
-import de.rayzs.tacticalmonsters.listener.MonsterHandler;
-import org.bukkit.entity.EntityType;
-import org.bukkit.event.HandlerList;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.Bukkit;
+import de.rayzs.tacticalmonsters.api.TacticalMonsters;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.HandlerList;
 
 public class TacticalMonstersLoader extends JavaPlugin {
 
@@ -27,28 +22,15 @@ public class TacticalMonstersLoader extends JavaPlugin {
             return;
         }
 
-        final long startTime = System.currentTimeMillis();
-
         api = new TacticalMonstersImpl(this);
         TacticalMonsters.setInstance(api);
 
+        api.reload();
 
-        final PluginManager manager = Bukkit.getServer().getPluginManager();
-        manager.registerEvents(new MonsterHandler(api), api.getPlugin());
-        manager.registerEvents(new AntiWitherCheeseHandler(api), api.getPlugin());
-
-
-        api.registerAttack(EntityType.WITHER_SKELETON, WitherSkeletonAttack.class);
-        api.registerAttack(EntityType.SKELETON, SkeletonAttack.class);
-        api.registerAttack(EntityType.ZOMBIE, ZombieAttack.class);
-        api.registerAttack(EntityType.SPIDER, SpiderAttack.class);
-
-
-        getLogger().info("Successfully loaded "
-                + api.getRegisteredAttacks().size()
-                + " monster attacks. ("
-                + (System.currentTimeMillis() - startTime) + "ms)"
-        );
+        api.registerAttack(WitherSkeletonAttack.class);
+        api.registerAttack(SkeletonAttack.class);
+        api.registerAttack(ZombieAttack.class);
+        api.registerAttack(SpiderAttack.class);
     }
 
     @Override
