2dbc9ac5b17679e24e8da629732c654cd4761eb8
rayzsyt <alihan290404@gmail.com>
2026-02-04 15:56:20 +0100

    Implement reload command

--- common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
+++ common/src/main/java/de/rayzs/tacticalmonsters/TacticalMonstersLoader.java
@@ -1,12 +1,14 @@
 package de.rayzs.tacticalmonsters;
 
 import de.rayzs.tacticalmonsters.attacks.WitherSkeletonAttack;
+import de.rayzs.tacticalmonsters.commands.ReloadCommand;
 import de.rayzs.tacticalmonsters.impl.TacticalMonstersImpl;
 import de.rayzs.tacticalmonsters.api.helper.VersionHelper;
 import de.rayzs.tacticalmonsters.attacks.SkeletonAttack;
 import de.rayzs.tacticalmonsters.attacks.SpiderAttack;
 import de.rayzs.tacticalmonsters.attacks.ZombieAttack;
 import de.rayzs.tacticalmonsters.api.TacticalMonsters;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.HandlerList;
 
@@ -24,6 +26,14 @@ public class TacticalMonstersLoader extends JavaPlugin {
 
         api = new TacticalMonstersImpl(this);
         TacticalMonsters.setInstance(api);
+
+
+        final PluginCommand pluginCommand = this.getCommand("tacticalmonstersreload");
+        if (pluginCommand != null) {
+            pluginCommand.setExecutor(new ReloadCommand(api));
+            pluginCommand.setTabCompleter(new ReloadCommand(api));
+        }
+
 
         api.reload();
 
