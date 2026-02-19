1f07f30c53abb72d4b0ba9d124c0fb7f8c732b13
Alihan Olgun <alihan.olgun@tuhh.de>
2026-02-19 03:30:37 +0100

    Added preparation to auto-create default files if folder does not exist

--- common/src/main/java/de/rayzs/tacticalmonsters/impl/configuration/ConfigProviderImpl.java
+++ common/src/main/java/de/rayzs/tacticalmonsters/impl/configuration/ConfigProviderImpl.java
@@ -1,11 +1,32 @@
 package de.rayzs.tacticalmonsters.impl.configuration;
 
 import de.rayzs.tacticalmonsters.api.configuration.*;
+
+import java.io.File;
 import java.util.*;
 
 public class ConfigProviderImpl implements ConfigProvider {
 
+    private final String defaultFolderPath = "plugins/TacticalMonsters";
     private final Map<String, Config> configs = new HashMap<>();
+
+
+    // Create default files if the folder does not exist yet.
+    public ConfigProviderImpl() {
+        final File folder = new File(defaultFolderPath);
+
+        if (folder.isDirectory()) {
+            return;
+        }
+
+        if (!folder.mkdirs()) {
+            throw new RuntimeException("Failed to create TacticalMonsters folder! (" + defaultFolderPath + ")");
+        }
+
+        // TO-DO:
+        // Create implementation to load all files from inside project
+        // into the plugins/TacticalMonsters folder.
+    }
 
     @Override
     public Config getOrCreate(final String fileName) {
@@ -14,14 +35,15 @@ public class ConfigProviderImpl implements ConfigProvider {
 
     @Override
     public Config getOrCreate(final String filePath, final String fileName) {
-        final String id = ((filePath != null) ? (filePath + "/") : "") + fileName;
+        final String path = defaultFolderPath + (filePath != null ? ("/" + filePath) : "");
+        final String id = path + "/" + fileName;
 
         Config config = configs.get(id);
         if (config != null) {
             return config;
         }
 
-        config = new ConfigImpl(filePath, fileName);
+        config = new ConfigImpl(path, fileName);
 
         configs.put(id, config);
         return config;
